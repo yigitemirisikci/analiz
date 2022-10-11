3e29767270801b0adc70a4e111fac765dfcb31e3
İlker Avcı <ikolomiko@gmail.com>
2022-10-12 00:55:42 +0300

    Lots of optimizing and refactoring

--- analysis_tools.py
+++ analysis_tools.py
@@ -1,7 +1,7 @@
 from csv import DictWriter
 from dataclasses import dataclass
 import os
-from typing import Any, List, Mapping
+from typing import Any, List, Mapping, Set
 
 
 @dataclass
@@ -18,25 +18,51 @@ class MethodSignature:
 
 
 class AnalysisWriter:
-    def __init__(self, headers: List[str], output: str) -> None:
-        file_exists = os.path.isfile(output)
-        self.file = open(output, mode='a+')
-        self.file.seek(0, 0)
-        self.writer = DictWriter(self.file, delimiter=',',
-                                 lineterminator='\n', fieldnames=headers)
+    def __init__(self, headers: List[str], path: str) -> None:
+        file_exists = os.path.isfile(path)
+        self.path = path
+        self.headers = headers
+
         if not file_exists:
-            self.writer.writeheader()
+            with open(self.path, "w") as file:
+                DictWriter(file, delimiter=",", lineterminator="\n",
+                           fieldnames=headers).writeheader()
 
-    def close(self) -> None:
-        self.file.close()
+    def writerow(self, rowdict: Mapping[str, Any]) -> None:
+        with open(self.path, 'a') as file:
+            writer = DictWriter(file, delimiter=",",
+                                lineterminator="\n", fieldnames=self.headers)
+            writer.writerow(rowdict)
 
-    def writerow(self, rowdict: Mapping[str, Any]) -> Any:
-        return self.writer.writerow(rowdict)
+    def write_signature(self, splitted_path: List[str], signature: MethodSignature, meth_list: List[str]) -> None:
+        self.writerow({'id': splitted_path[0] + "+" + splitted_path[1], 'artifact_id': splitted_path[0], 'group_id': splitted_path[1],
+                       "version": splitted_path[2][:-4], 'signature': f"{signature.classname}->{signature.methodname}", 'method': meth_list})
 
-    def write_signature(self, splitted_path: List[str], signature: MethodSignature, meth_list: List[str]) -> Any:
-        return self.writerow({'id': splitted_path[0] + "+" + splitted_path[1], 'artifact_id': splitted_path[0], 'group_id': splitted_path[1],
-                              "version": splitted_path[2][:-4], 'signature': f"{signature.classname}->{signature.methodname}", 'method': meth_list})
+    def write_permission(self, splitted_path: List[str], perm, meth, meth_list) -> None:
+        self.writerow({'id': splitted_path[0] + "+" + splitted_path[1], 'artifact_id': splitted_path[0], 'group_id': splitted_path[1],
+                       "version": splitted_path[2][:-4], 'permission': perm, 'api': meth.full_name, 'method': meth_list})
 
-    def write_permission(self, splitted_path: List[str], perm, meth, meth_list) -> Any:
-        return self.writerow({'id': splitted_path[0] + "+" + splitted_path[1], 'artifact_id': splitted_path[0], 'group_id': splitted_path[1],
-                                    "version": splitted_path[2][:-4], 'permission': perm, 'api': meth.full_name, 'method': meth_list})
+
+class Blacklist:
+    def __init__(self, path: str = None) -> None:
+        if path == None:
+            path = "./blacklist.txt"
+
+        self.path = path
+
+        self.blacklisted: Set[str] = set()
+        with open(path, "r") as file:
+            for line in file:
+                self.blacklisted.add(line.strip())
+
+    def write_all(self) -> None:
+        with open(self.path, "w") as file:
+            for item in self.blacklisted:
+                file.write(item + "\n")
+
+    def add(self, item: str) -> None:
+        self.blacklisted.add(item)
+        self.write_all()
+
+    def contains(self, item: str) -> bool:
+        return item in self.blacklisted
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
+import gc
 import os
 import subprocess
 from typing import Any, List, Tuple
 from zipfile import ZipFile
-import shutil
 from androguard.misc import AnalyzeDex
 from androguard.core.analysis.analysis import Analysis
 import os.path
 import json
 from libmetadata import LibMetadata
-from analysis_tools import AnalysisWriter, JavascriptResult, MethodSignature
+from analysis_tools import AnalysisWriter, JavascriptResult, MethodSignature, Blacklist
 
 """
 Baslangic dosya formati:
@@ -26,10 +26,10 @@ Baslangic dosya formati:
         
 """
 
-lib_path = "../libsec-scraper/updated-libs"
+lib_path = "libs"  # "../libsec-scraper/updated-libs"
 dex_path = "./dex_files"
 blacklist_file_path = "./blacklist.txt"
-metadata_path = "../libsec-scraper/libdata"
+metadata_path = "metadata"  # "../libsec-scraper/libdata"
 
 
 writer_permission = AnalysisWriter(['id', 'artifact_id', 'group_id', 'version', 'permission', 'api', 'method'],
@@ -42,13 +42,6 @@ writer_reflection = AnalysisWriter(['id', 'artifact_id', 'group_id', 'version', 
                                    "reflection.csv")
 writer_inspackages = AnalysisWriter(['id', 'artifact_id', 'group_id', 'version', 'signature', 'method'],
                                     "installed_packages.csv")
-
-
-file_blacklist = open(blacklist_file_path, mode='a+')
-file_blacklist.seek(0, 0)
-
-blacklist = file_blacklist.readlines()
-blacklist = [s.strip() for s in blacklist]
 
 
 def check_classloader(analysis: Analysis) -> bool:
@@ -85,14 +78,14 @@ def check_installed_packages(analysis: Analysis) -> bool:
             "Landroid/content/pm/PackageManager;", "getApplicationInfo")))
 
 
-def convertJARtoDEX(file):
-    if file.endswith('.jar') and not file in blacklist:
-        result = subprocess.call(
+def convertJARtoDEX(file: str, blacklist: Blacklist):
+    if file.endswith('.jar') and not blacklist.contains(file):
+        subprocess.call(
             ["./dex-tools/d2j-jar2dex.sh", file, "-o", file[:-4] + ".dex"])
         file_exists = os.path.isfile(file[:-4] + ".dex")
-        if file_exists == False:
-            file_blacklist.write(file + "\n")
-            file_blacklist.flush()
+        if not file_exists:
+            blacklist.add(file)
+
     return file[:-4] + ".dex"
 
 
@@ -114,85 +107,118 @@ def check_permissions(analysis: Analysis, splitted_path: List[str]) -> None:
         for _, m, _ in meth.get_xref_from():
             meth_list.append(m.full_name)
 
-        writer_permission.write_permission(splitted_path, perm, meth, meth_list)
+        writer_permission.write_permission(
+            splitted_path, perm, meth, meth_list)
 
 
 def analyzeDEXfiles():
     for root, directories, files in os.walk(lib_path):
         for file in files:
-            if file.endswith('.dex'):
-                path = root + "/" + file
-                path = path.replace("/", "+")
-                splitted_path = path.split("+")
-                splitted_path = splitted_path[-3:]
+            if not file.endswith('.dex'):
+                continue
 
-                analysis_results: Tuple[Any, Any, Analysis] = AnalyzeDex(
-                    filename=root + "/" + file)
-                a, b, analysis = analysis_results
+            path = root + "/" + file
+            path = path.replace("/", "+")
+            splitted_path = path.split("+")
+            splitted_path = splitted_path[-3:]
 
-                # Javascript
-                signature_jsenabled = MethodSignature(
-                    "Landroid.webkit.WebSettings;", "setJavaScriptEnabled")
-                signature_jsinterface = MethodSignature(
-                    "Landroid.webkit.WebView;", "addJavascriptInterface")
-                signature_jseval = MethodSignature(
-                    "Landroid.webkit.WebView;", "evaluateJavascript"
-                )
+            analysis_results: Tuple[Any, Any, Analysis] = AnalyzeDex(
+                filename=root + "/" + file)
+            a, b, analysis = analysis_results
 
-                check_signature(analysis, signature_jsenabled,
-                                splitted_path, writer_javascript)
-                check_signature(analysis, signature_jsinterface,
-                                splitted_path, writer_javascript)
-                check_signature(analysis, signature_jseval,
-                                splitted_path, writer_javascript)
+            # Javascript
+            signature_jsenabled = MethodSignature(
+                "Landroid.webkit.WebSettings;", "setJavaScriptEnabled")
+            signature_jsinterface = MethodSignature(
+                "Landroid.webkit.WebView;", "addJavascriptInterface")
+            signature_jseval = MethodSignature(
+                "Landroid.webkit.WebView;", "evaluateJavascript"
+            )
 
-                # Class loader
-                sign_cloader1 = MethodSignature("Ldalvik/system/DexClassLoader;", "loadClass()")
-                sign_cloader2 = MethodSignature("Ldalvik/system/PathClassLoader;", "loadClass()")
-                sign_cloader3 = MethodSignature("Ljava/net/URLClassLoader;", "loadClass()")
-                sign_cloader4 = MethodSignature("Ldalvik/system/DelegateLastClassLoader;", "loadClass()")
-                sign_cloader5 = MethodSignature("Ldalvik/system/InMemoryDexClassLoader;", "loadClass()")
+            check_signature(analysis, signature_jsenabled,
+                            splitted_path, writer_javascript)
+            check_signature(analysis, signature_jsinterface,
+                            splitted_path, writer_javascript)
+            check_signature(analysis, signature_jseval,
+                            splitted_path, writer_javascript)
 
-                check_signature(analysis, sign_cloader1, splitted_path, writer_classloader)
-                check_signature(analysis, sign_cloader2, splitted_path, writer_classloader)
-                check_signature(analysis, sign_cloader3, splitted_path, writer_classloader)
-                check_signature(analysis, sign_cloader4, splitted_path, writer_classloader)
-                check_signature(analysis, sign_cloader5, splitted_path, writer_classloader)
+            # Class loader
+            sign_cloader1 = MethodSignature(
+                "Ldalvik/system/DexClassLoader;", "loadClass()")
+            sign_cloader2 = MethodSignature(
+                "Ldalvik/system/PathClassLoader;", "loadClass()")
+            sign_cloader3 = MethodSignature(
+                "Ljava/net/URLClassLoader;", "loadClass()")
+            sign_cloader4 = MethodSignature(
+                "Ldalvik/system/DelegateLastClassLoader;", "loadClass()")
+            sign_cloader5 = MethodSignature(
+                "Ldalvik/system/InMemoryDexClassLoader;", "loadClass()")
 
-                # Reflection
-                sign_ref1 = MethodSignature("Ljava/lang/reflect/", "")
-                sign_ref2 = MethodSignature("Lkotlin/reflect/", "")
+            check_signature(analysis, sign_cloader1,
+                            splitted_path, writer_classloader)
+            check_signature(analysis, sign_cloader2,
+                            splitted_path, writer_classloader)
+            check_signature(analysis, sign_cloader3,
+                            splitted_path, writer_classloader)
+            check_signature(analysis, sign_cloader4,
+                            splitted_path, writer_classloader)
+            check_signature(analysis, sign_cloader5,
+                            splitted_path, writer_classloader)
 
-                check_signature(analysis, sign_ref1, splitted_path, writer_reflection)
-                check_signature(analysis, sign_ref2, splitted_path, writer_reflection)
-                
-                # Installed packages
-                sign_insp1 = MethodSignature("Landroid/content/pm/PackageManager;", "getInstallSourceInfo")
-                sign_insp2 = MethodSignature("Landroid/content/pm/PackageManager;", "getInstalledApplications")
-                sign_insp3 = MethodSignature("Landroid/content/pm/PackageManager;", "getInstalledPackages")
-                sign_insp4 = MethodSignature("Landroid/content/pm/PackageManager;", "getPackageInfo")
-                sign_insp5 = MethodSignature("Landroid/content/pm/PackageManager;", "getApplicationInfo")
+            # Reflection
+            sign_ref1 = MethodSignature("Ljava/lang/reflect/", "")
+            sign_ref2 = MethodSignature("Lkotlin/reflect/", "")
 
-                check_signature(analysis, sign_insp1, splitted_path, writer_inspackages)
-                check_signature(analysis, sign_insp2, splitted_path, writer_inspackages)
-                check_signature(analysis, sign_insp3, splitted_path, writer_inspackages)
-                check_signature(analysis, sign_insp4, splitted_path, writer_inspackages)
-                check_signature(analysis, sign_insp5, splitted_path, writer_inspackages)
+            check_signature(analysis, sign_ref1,
+                            splitted_path, writer_reflection)
+            check_signature(analysis, sign_ref2,
+                            splitted_path, writer_reflection)
 
-                # dangerous permissions
-                check_permissions(analysis, splitted_path)
+            # Installed packages
+            sign_insp1 = MethodSignature(
+                "Landroid/content/pm/PackageManager;", "getInstallSourceInfo")
+            sign_insp2 = MethodSignature(
+                "Landroid/content/pm/PackageManager;", "getInstalledApplications")
+            sign_insp3 = MethodSignature(
+                "Landroid/content/pm/PackageManager;", "getInstalledPackages")
+            sign_insp4 = MethodSignature(
+                "Landroid/content/pm/PackageManager;", "getPackageInfo")
+            sign_insp5 = MethodSignature(
+                "Landroid/content/pm/PackageManager;", "getApplicationInfo")
 
-                # Genel sonuçlar (şimdilik kullanılmadı)
+            check_signature(analysis, sign_insp1,
+                            splitted_path, writer_inspackages)
+            check_signature(analysis, sign_insp2,
+                            splitted_path, writer_inspackages)
+            check_signature(analysis, sign_insp3,
+                            splitted_path, writer_inspackages)
+            check_signature(analysis, sign_insp4,
+                            splitted_path, writer_inspackages)
+            check_signature(analysis, sign_insp5,
+                            splitted_path, writer_inspackages)
+
+            # dangerous permissions
+            check_permissions(analysis, splitted_path)
+
+            # Genel sonuçlar (şimdilik kullanılmadı)
+            """
                 uses_classloader = check_classloader(analysis)
                 javascript_result = check_javascript(analysis)
                 uses_reflection = check_reflection(analysis)
                 uses_pm = check_installed_packages(analysis)
+                """
+
+            del analysis_results
+            del analysis
+            gc.collect()
 
 
 def get_metadata_paths() -> List[str]:
     metadata_path_list: List[str] = []
     for root, _, files in os.walk(metadata_path):
         for file in files:
+            if file != "metadata.json":
+                continue
             metadata_path_list.append(root+"/"+file)
 
     return metadata_path_list
@@ -218,49 +244,39 @@ def get_lib_paths(metadata_paths: List[str]) -> List[str]:
     return lib_paths
 
 
-metadata_paths = get_metadata_paths()
-lib_paths = get_lib_paths(metadata_paths)
+def main() -> None:
+    blacklist = Blacklist(blacklist_file_path)
+    metadata_paths = sorted(get_metadata_paths())
+    lib_paths = get_lib_paths(metadata_paths)
 
-dex_paths = []
-for item_lib_path in lib_paths:
-    file_exists2 = os.path.isfile(
-        item_lib_path[:-4] + ".aar") or os.path.isfile(item_lib_path[:-4] + ".jar")
-    if(file_exists2):
-        continue
-    item_lib_path = lib_path+"/"+item_lib_path
-    file_exists = os.path.isfile(item_lib_path[:-4] + ".dex")
-    if(file_exists):
-        dex_paths.append(item_lib_path[:-4] + ".dex")
-        continue
-    try:
-        if item_lib_path[-3:] == "aar":
-            pre, ext = os.path.splitext(item_lib_path)
-            # copying existing aar
-            shutil.copyfile(item_lib_path, item_lib_path[:-4] + "-copy.aar")
-            # converting copied .aar to .zip to extract 'classes.jar' inside of it
-            os.rename(item_lib_path[:-4] + "-copy.aar",
-                      item_lib_path[:-4] + ".zip")
-            with ZipFile(item_lib_path[:-4] + ".zip", 'r') as zipObj:
-                listOfiles = zipObj.namelist()
-                for element in listOfiles:
-                    if element == "classes.jar":
-                        zipObj.extract(element, "./")
-                        os.rename("./classes.jar", item_lib_path[:-4] + ".jar")
-                        dex_path = convertJARtoDEX(item_lib_path[:-4] + ".jar")
-                        dex_paths.append(dex_path)
-        elif item_lib_path[-3:] == "jar":
-            dex_path = convertJARtoDEX(item_lib_path)
-            dex_paths.append(dex_path)
-    except:
-        file_blacklist.write(item_lib_path[:-4] + ".jar\n")
-        file_blacklist.flush()
+    for item_lib_path in lib_paths:
+        item_lib_path = lib_path+"/"+item_lib_path
+
+        if blacklist.contains(item_lib_path):
+            continue
+
+        dex_exists = os.path.isfile(item_lib_path[:-4] + ".dex")
+        if dex_exists:
+            continue
+
+        try:
+            if item_lib_path[-3:] == "aar":
+                with ZipFile(item_lib_path, 'r') as zipObj:
+                    listOfiles = zipObj.namelist()
+                    for element in listOfiles:
+                        if element == "classes.jar":
+                            zipObj.extract(element, "./")
+                            os.rename("./classes.jar",
+                                      item_lib_path[:-4] + ".jar")
+                            break
+
+            convertJARtoDEX(item_lib_path[:-4] + ".jar", blacklist)
+
+        except:
+            blacklist.add(item_lib_path[:-4] + ".jar")
+
+    analyzeDEXfiles()
 
 
-analyzeDEXfiles()
-
-writer_classloader.close()
-writer_inspackages.close()
-writer_javascript.close()
-writer_permission.close()
-writer_reflection.close()
-file_blacklist.close()
+if __name__ == '__main__':
+    main()
